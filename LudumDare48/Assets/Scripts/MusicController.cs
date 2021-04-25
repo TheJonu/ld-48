978cfaec3624b06a0cfa474152e121e2d9397d68
edgeboyo <mkolybko@gmail.com>
2021-04-25 12:53:36 +0100

    Added simple music handler to the player prefab

--- LudumDare48/Assets/Scripts/MusicController.cs
+++ LudumDare48/Assets/Scripts/MusicController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour
 {
+    public AudioClip[] clips;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioSource source = gameObject.AddComponent<AudioSource>() as AudioSource;
+        source.clip = clips[0];
+        source.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
