06e781e2268a2d31777f527ccb54a34082868056
aler-aler <glimglam@protonmail.com>
2021-04-26 23:13:21 +0200

    Dynamusic fixed

--- LudumDare48/Assets/Scripts/InsanityManager.cs
+++ LudumDare48/Assets/Scripts/InsanityManager.cs
@@ -9,8 +9,6 @@ public class InsanityManager : MonoBehaviour
     public Material[] materialSelection;
     public Material[] enemyMaterialSelection;
 
-    public GameObject playerLocation;
-
     public float insanityPercent;
     public float enemyInsanityPercent;
 
@@ -61,10 +59,9 @@ public class InsanityManager : MonoBehaviour
                 } 
                 else
                 {
-                    Debug.Log("Insanity increased");
                     insanityPercent += insanityInc / 30.0f + insanityPercent / 30.0f;
                     enemyInsanityInc += enemyInsanityInc / 30.0f + enemyInsanityInc / 30.0f;
-                    playerLocation.GetComponent<MusicController>().Next();
+                    Camera.main.GetComponent<MusicController>().Next();
                 }
             }
             foreach (GameObject gm in lev.GetSprites())
